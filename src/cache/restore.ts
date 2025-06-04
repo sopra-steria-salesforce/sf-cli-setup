9f3877940210197291f88fe508b43c35fb59794d
John Arne Føreland <john.foreland@me.com>
2025-06-04 13:16:27 +0200

    Made SF CLI upper case in log output

--- src/cache/restore.ts
+++ src/cache/restore.ts
@@ -4,7 +4,7 @@ import * as tc from '@actions/tool-cache'
 import { cachePaths, primaryKey } from './constants.js'
 
 export const getCachedSfCli = async (): Promise<string | void> => {
-  core.info(`Checking for sf cli with version ${primaryKey} in GitHub cache...`)
+  core.info(`Checking for SF CLI with version ${primaryKey} in GitHub cache...`)
 
   const cacheKey = await cache.restoreCache(cachePaths, primaryKey)
   core.info(cacheKey ? `Cache restored (key: ${cacheKey})` : 'Cache not found, will download from npm.')
