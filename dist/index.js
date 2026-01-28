87da0dde91fa7bc816560a7bf105931b2182b58d
John Arne Føreland <john.foreland@me.com>
2026-01-28 14:32:10 +0100

    Revert changes

--- dist/index.js
+++ dist/index.js
@@ -953,13 +953,8 @@ var toolCacheExports = requireToolCache();
 const getCachedSfCli = async () => {
     coreExports.info(`Checking for SF CLI with version ${primaryKey} in GitHub cache...`);
     const cacheKey = await cacheExports.restoreCache(cachePaths, primaryKey);
-    if (cacheKey) {
-        coreExports.info(`Cache restored (key: ${cacheKey})`);
-        // Return the path where cache was restored (includes version in the path)
-        return `${cachePaths[0]}/${primaryKey.split('@')[1]}/x64`;
-    }
-    coreExports.info('Cache not found, will download from npm.');
-    return undefined;
+    coreExports.info(cacheKey ? `Cache restored (key: ${cacheKey})` : 'Cache not found, will download from npm.');
+    return toolCacheExports.find('sf-cli', primaryKey);
 };
 
 var ioExports = requireIo();
--- src/cache/restore.ts
+++ src/cache/restore.ts
@@ -1,18 +1,13 @@
 import * as cache from '@actions/cache'
 import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
 import { cachePaths, primaryKey } from './constants.js'
 
 export const getCachedSfCli = async (): Promise<string | void> => {
   core.info(`Checking for SF CLI with version ${primaryKey} in GitHub cache...`)
 
   const cacheKey = await cache.restoreCache(cachePaths, primaryKey)
+  core.info(cacheKey ? `Cache restored (key: ${cacheKey})` : 'Cache not found, will download from npm.')
 
-  if (cacheKey) {
-    core.info(`Cache restored (key: ${cacheKey})`)
-    // Return the path where cache was restored (includes version in the path)
-    return `${cachePaths[0]}/${primaryKey.split('@')[1]}/x64`
-  }
-
-  core.info('Cache not found, will download from npm.')
-  return undefined
+  return tc.find('sf-cli', primaryKey)
 }
